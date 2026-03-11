08fb32dac93f51207442152a815a258e7007b8ea
Mahmuda Binte Kabir <24524203082@student.bup.edu.bd>
2026-03-11 11:47:24 +0600

    Create task4.c

---
+++ 1-2/cse-1204-structured-programming-lab/lab1/task4.c
@@ -0,0 +1,12 @@
+/*
+Print a quotation using printf, ensuring the quote is
+correctly formatted with quotation marks and line breaks.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\"Programming isn't about what you know; it's about what\nyou can figure out.\" - Chris Pine");
+    return 0;
+}
