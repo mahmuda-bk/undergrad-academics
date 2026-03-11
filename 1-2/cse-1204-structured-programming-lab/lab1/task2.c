cf090fb360ef347f696fbcf1b68661ae732dd123
Mahmuda Binte Kabir <24524203082@student.bup.edu.bd>
2026-03-11 11:44:55 +0600

    Create task2.c

---
+++ 1-2/cse-1204-structured-programming-lab/lab1/task2.c
@@ -0,0 +1,20 @@
+/*
+Show the multiplication table of 9. (N.B. Don’t use loop.)
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("9 x 1 = %d\n", 9*1);
+    printf("9 x 2 = %d\n", 9*2);
+    printf("9 x 3 = %d\n", 9*3);
+    printf("9 x 4 = %d\n", 9*4);
+    printf("9 x 5 = %d\n", 9*5);
+    printf("9 x 6 = %d\n", 9*6);
+    printf("9 x 7 = %d\n", 9*7);
+    printf("9 x 8 = %d\n", 9*8);
+    printf("9 x 9 = %d\n", 9*9);
+    printf("9 x 10 = %d\n", 9*10);
+    return 0;
+}
