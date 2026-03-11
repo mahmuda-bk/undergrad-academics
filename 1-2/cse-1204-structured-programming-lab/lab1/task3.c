37c0e36d4b5d82114e75b78cadcef159b7c73049
Mahmuda Binte Kabir <24524203082@student.bup.edu.bd>
2026-03-11 11:46:06 +0600

    Create task3.c

---
+++ 1-2/cse-1204-structured-programming-lab/lab1/task3.c
@@ -0,0 +1,16 @@
+/*
+Write a C program using only printf function to show a Triangle.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Triangle:\n");
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n");
+    return 0;
+}
