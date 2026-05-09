966affb26ec24b04ebafb061561065e12824b87a
Mahmuda Binte Kabir <24524203082@student.bup.edu.bd>
2026-05-09 11:56:04 +0600

    Create polar_nrz_i.m

---
+++ 3-1/cse-3110-data-comm-lab/lab04/polar_nrz_i.m
@@ -0,0 +1,26 @@
+clc;
+clear;
+close all;
+
+bits = [1 0 1 1 0 1 1 0 1 1 1 1 1 0 0 1 1 0 1 1 0 0 1 1 1 0 1 0 1 0 1 0 1 0];
+
+fs = 100; t = 0:1/fs:length(bits);
+y = zeros(1, length(t));
+level = 1;
+
+for i = 1:length(bits)
+   if bits(i) == 1
+       level = -level;
+   end
+   y((i-1)*fs+1 : i*fs) = level;
+end
+
+figure;
+subplot(2,1,1); 
+stem(bits, 'LineWidth', 2); 
+title('Digital Data');
+
+subplot(2,1,2); 
+plot(t, y, 'LineWidth', 2); 
+title('Polar NRZ-I Signal');
+grid on;
