88a510443510c7ce83e0e2007a3019d74d7892ad
Mahmuda Binte Kabir <24524203082@student.bup.edu.bd>
2026-05-09 11:57:05 +0600

    Create bipolar_ami.m

---
+++ 3-1/cse-3110-data-comm-lab/lab04/bipolar_ami.m
@@ -0,0 +1,27 @@
+clc;
+clear;
+close all;
+
+bits = [1 0 1 1 0 1 1 0 1 1 1 1 1 0 0 1 1 0 1 1 0 0 1 1 1 0 1 0 1 0 1 0 1 0];
+
+fs = 100;
+t = 0:1/fs:length(bits);
+y = zeros(1, length(t));
+last_one = -1;
+
+for i = 1:length(bits)
+   if bits(i) == 1
+       last_one = -last_one;
+       y((i-1)*fs+1 : i*fs) = last_one;
+   end
+end
+
+figure;
+subplot(2,1,1); 
+stem(bits, 'LineWidth', 2); 
+title('Digital Data');
+
+subplot(2,1,2); 
+plot(t, y, 'LineWidth', 2); 
+title('Bipolar AMI Signal');
+grid on;
